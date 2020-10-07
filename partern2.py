e1c25790f4c7c98d94c485c14a11b0d7f8c6d442
Derrick Park <derrick8689@gmail.com>
2020-10-07 15:33:14 -0700

    Fix typos

--- partern2.py
+++ partern2.py
@@ -2,10 +2,12 @@ import turtle
 screen = turtle.Screen()
 t = turtle.Turtle()
 t.shape("turtle")
-def draw_square(lenght: int):
+
+def draw_square(length: int):
     for i in range(4):
-        t.forward(lenght)
+        t.forward(length)
         t.right(90)
+
 def exercise_2():
     draw_square(100)
     t.up()
@@ -14,4 +16,5 @@ def exercise_2():
     t.setheading(-45)
     draw_square(100)
     screen.exitonclick()
+
 exercise_2()
--- partern4.py
+++ partern4.py
@@ -2,25 +2,25 @@ import turtle
 screen = turtle.Screen()
 t = turtle.Turtle()
 t.shape("turtle")
-lenght = 100
+length = 100
 angle = -60
 def exercise_4():
     for i in range(6):
-        t.forward(lenght)
+        t.forward(length)
         t.setheading(angle*(i+1))
     t.setheading(angle)
-    t.forward(2*lenght)
+    t.forward(2*length)
     t.setheading(-3*angle)
     t.up()
-    t.forward(lenght)
+    t.forward(length)
     t.setheading(-1*angle)
     t.down()
-    t.forward(2*lenght)
+    t.forward(2*length)
     t.up()
     t.setheading(angle)
-    t.forward(lenght)
+    t.forward(length)
     t.setheading(3 * angle)
     t.down()
-    t.forward(2 * lenght)
+    t.forward(2 * length)
     screen.exitonclick()
 exercise_4()
